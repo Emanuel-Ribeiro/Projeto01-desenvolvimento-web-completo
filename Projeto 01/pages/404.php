74b336be000838245b15c6de17aac8b751d030e0
Emanuel Ribeiro <lima.ribeiro.emanuel@gmail.com>
2020-10-11 12:43:36 -0300

    pagina do erro 404

---
+++ Projeto 01/pages/404.php
@@ -0,0 +1,8 @@
+<section class="erro-404">
+  <div class="center">
+    <div class="wrapper-404">
+      <h2><i style="padding: 0 10px;" class="fa fa-times"></i> Pagina não encontrada</h2>
+      <p>Deseja voltar para a <a href="<?php echo INCLUDE_PATH; ?>">pagina inicial</a>?</p>
+    </div><!--div wrappe 404-->
+  </div><!--div center-->
+</section><!--section 404-->
---
+++ Projeto 01/pages/home.php
@@ -0,0 +1,79 @@
+<section class="banner-principal">
+    <div class="overlay"></div><!--div overlay-->
+    <div class="center">
+      <form>
+        <h2>Qual é o seu melhor e-mail?</h2>
+        <input type="email" name="email" required>
+        <input type="submit" name="acao" value="Cadastrar!">
+      </form>
+    </div><!--div center-->
+  </section><!--banner principal-->
+
+  <section class="descricao-autor">
+    <div class="center">
+    <div class="w50 left">
+        <h2>Emanuel Ribeiro</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non odio metus. Mauris quis ligula non elit pulvinar sodales. Fusce hendrerit, felis at aliquet semper, nibh mi dignissim sapien, sit amet rhoncus urna tellus et lacus. Nunc tempor, turpis non consequat ultrices, ipsum augue commodo mauris, eu tincidunt metus ipsum non arcu. Donec placerat nisl nisl, quis posuere leo maximus quis. Nulla hendrerit lacus lacus, in imperdiet libero bibendum ac.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non odio metus. Mauris quis ligula non elit pulvinar sodales. Fusce hendrerit, felis at aliquet semper, nibh mi dignissim sapien, sit amet rhoncus urna tellus et lacus. Nunc tempor, turpis non consequat ultrices, ipsum augue commodo mauris, eu tincidunt metus ipsum non arcu. Donec placerat nisl nisl, quis posuere leo maximus quis. Nulla hendrerit lacus lacus, in imperdiet libero bibendum ac.</p>
+    </div><!--div w50-->
+    <div class="w50 left">
+        <img class="right" src="<?php echo INCLUDE_PATH; ?>images/foto.jpg">
+    </div><!--div w50-->
+    <div class="clear"></div><!--div clear-->
+    </div><!--div center-->
+  </section><!--descriçao-autor-->
+
+  <section class="especialidades">
+    <div class="center">
+      <h2 class="title">Especialidades</h2>
+      <div class="w33 left box-especialidade">
+        <h3><i class="fa fa-css3" aria-hidden="true"></i></h3>
+        <h4>CSS 3</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non odio metus. Mauris quis ligula non elit pulvinar sodales. Fusce hendrerit, felis at aliquet semper, nibh mi dignissim sapien, sit amet rhoncus urna tellus et lacus.</p>
+      </div><!--div box-especialidade-->
+      <div class="w33 left box-especialidade">
+        <h3><i class="fa fa-html5" aria-hidden="true"></i></h3>
+        <h4>HTML 5</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non odio metus. Mauris quis ligula non elit pulvinar sodales. Fusce hendrerit, felis at aliquet semper, nibh mi dignissim sapien, sit amet rhoncus urna tellus et lacus.</p>
+      </div><!--div box-especialidade-->
+      <div class="w33 left box-especialidade">
+        <h3><i class="fab fa-php" aria-hidden="true"></i></h3>
+        <h4>PHP</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non odio metus. Mauris quis ligula non elit pulvinar sodales. Fusce hendrerit, felis at aliquet semper, nibh mi dignissim sapien, sit amet rhoncus urna tellus et lacus.</p>
+      </div><!--div box-especialidade-->
+      <div class="clear"></div><!--div clear-->
+    </div><!--div center-->
+  </section><!--section especialidades-->
+
+  <section class="extras">
+
+    <div class="center">
+      <div class="w50 left depoimentos-container">
+        <h2 class="title">Certificados</h2>
+        <div class="depoimento-single">
+          <p class="nome-autor">Titulo do certificado</p>
+          <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non odio metus. Mauris quis ligula non elit pulvinar sodales. Fusce hendrerit, felis at aliquet semper, nibh mi dignissim sapien, sit amet rhoncus urna tellus et lacus.</p>
+        </div><!--div depoimento-single-->
+        <div class="depoimento-single">
+          <p class="nome-autor">Titulo do certificado</p>
+          <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non odio metus. Mauris quis ligula non elit pulvinar sodales. Fusce hendrerit, felis at aliquet semper, nibh mi dignissim sapien, sit amet rhoncus urna tellus et lacus.</p>
+        </div><!--div depoimento-single-->
+        <div class="depoimento-single">
+          <p class="nome-autor">Titulo do certificado</p>
+          <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non odio metus. Mauris quis ligula non elit pulvinar sodales. Fusce hendrerit, felis at aliquet semper, nibh mi dignissim sapien, sit amet rhoncus urna tellus et lacus.</p>
+        </div><!--div depoimento-single-->
+      </div><!--div w50-->
+
+      <div class="w50 left servicos-container">
+        <h2 class="title">Serviços</h2>
+        <div class="servicos">
+        <ul>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non odio metus. Mauris quis ligula non elit pulvinar sodales. Fusce hendrerit, felis at aliquet semper, nibh mi dignissim sapien, sit amet rhoncus urna tellus et lacus.</li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non odio metus. Mauris quis ligula non elit pulvinar sodales. Fusce hendrerit, felis at aliquet semper, nibh mi dignissim sapien, sit amet rhoncus urna tellus et lacus.</li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non odio metus. Mauris quis ligula non elit pulvinar sodales. Fusce hendrerit, felis at aliquet semper, nibh mi dignissim sapien, sit amet rhoncus urna tellus et lacus.</li>
+        </ul>
+        </div><!--div servicos-->
+      </div><!--div w50-->
+      <div class="clear"></div><!--div clear-->
+    </div><!--div center-->
+  </section><!--section extras-->
